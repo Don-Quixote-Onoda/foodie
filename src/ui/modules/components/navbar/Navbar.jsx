73b484a776ffd5df0ca20fbb70e7a69c62cfaaa4
Jay Mark Nicolas <jmnicolas4me@gmail.com>
2024-02-13 08:48:25 +0800

    update

---
+++ src/ui/modules/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../../../assets/images/Logo.svg'
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import HomeIcon from "@mui/icons-material/Home"
+import InfoIcon from "@mui/icons-material/Info"
+import 
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
--- src/ui/pages/home/Home.jsx
+++ src/ui/pages/home/Home.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Header from '../../modules/blocks/header/Header'
 import Footer from '../../modules/blocks/footer/Footer'
 import Main from '../../modules/blocks/main/Main'
+import Navbar from '../../modules/components/navbar/Navbar'
 
 function Home() {
   return (
-    <div>
-        <Header />
-        <Main>Hello</Main>
-        <Footer />
+    <div className='home-container'>
+      <Navbar />
     </div>
   )
 }
